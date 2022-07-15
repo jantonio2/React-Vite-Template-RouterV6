bea10e795cc77f36dd8f2e948e7ebf1757245c35
jantonio2 <jesusrochavedia@gmail.com>
2022-07-14 23:34:11 -0400

    Creando el shoppingCart

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -22,7 +22,21 @@ export const ShoppingPage = () => {
   const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
 
   const onProductCountChange = ({ count, product }: { count:number, product: Product }) => {
-    console.log('OnProductCountChange');
+
+    setShoppingCart( oldShoppingCart => {
+
+      if( count === 0 ){
+        
+        const { [product.id]:toDelete, ...rest } = oldShoppingCart;
+
+        return { ...rest }
+      }
+
+      return {
+        ...oldShoppingCart,
+        [ product.id ]: { ...product, count }
+      }
+    })
   }
 
   return (
@@ -71,6 +85,11 @@ export const ShoppingPage = () => {
           </ProductCard>
         </div>
 
+        <div>
+          <code>
+            { JSON.stringify(shoppingCart, null, 5) }
+          </code>
+        </div>
     </div>
   );
 }
