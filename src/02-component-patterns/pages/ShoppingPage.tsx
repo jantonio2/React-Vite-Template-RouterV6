cb28a84fcadc9345072f971dd4e91e0051410ba9
jantonio2 <jesusrochavedia@gmail.com>
2022-07-14 23:58:19 -0400

    Mostrando items del carrito de compras

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -65,31 +65,29 @@ export const ShoppingPage = () => {
         }
 
       </div>
-
-        <div className='shopping-cart'>
-          <ProductCard
-            product={ product2 }
-            className='bg-dark text-white'
-            style={{ width: '100px' }}
-          >
-            <ProductImage className='text-bold custom-image' />
-            <ProductButtons className='custom-buttons' />
-          </ProductCard>
-          <ProductCard
-            product={ product1 }
-            className='bg-dark text-white'
-            style={{ width: '100px' }}
-          >
-            <ProductImage className='text-bold custom-image' />
-            <ProductButtons className='custom-buttons' />
-          </ProductCard>
-        </div>
-
-        <div>
-          <code>
-            { JSON.stringify(shoppingCart, null, 5) }
-          </code>
-        </div>
+        
+      <div className='shopping-cart'>
+        {
+          Object.entries(shoppingCart).map( ([key, product]) => (
+            <ProductCard
+              key={ key }
+              product={ product }
+              className='bg-dark text-white'
+              style={{ width: '100px' }}
+            >
+              <ProductImage className='text-bold custom-image' />
+              <ProductButtons 
+                className='custom-buttons' 
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center'
+                }}
+              />
+            </ProductCard>
+          ))
+        }
+          
+      </div>
     </div>
   );
 }
