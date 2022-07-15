148d5a623ffda5eb94a8b9632f5fd09fbfeefbc5
jantonio2 <jesusrochavedia@gmail.com>
2022-07-15 13:17:28 -0400

    Aplicando control props, evaluando si el componente es controlado o no

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,13 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useProductArgs } from '../interfaces/interfaces';
 
 export const useProduct = ( { onChange, product, value = 0 }: useProductArgs ) => {
   const [counter, setCounter] = useState(value);
+
+  const isControlled = useRef( !!onChange );
+
   const increaseBy = ( value: number ) => {
+
+    if( isControlled.current ) {
+      return onChange!({ count: value, product });
+    }
 
     const newValue = Math.max( counter + value, 0);
     setCounter( newValue );
-
     onChange && onChange( { count: newValue, product: product } );
   }
 
