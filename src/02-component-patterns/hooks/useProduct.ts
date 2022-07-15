00faf8a33629a642fc36e225a47514a392254da5
jantonio2 <jesusrochavedia@gmail.com>
2022-07-15 13:53:46 -0400

    Refactorizando a una versión alternativa de control props

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,16 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useProductArgs } from '../interfaces/interfaces';
 
 export const useProduct = ( { onChange, product, value = 0 }: useProductArgs ) => {
   const [counter, setCounter] = useState(value);
 
-  const isControlled = useRef( !!onChange );
-
   const increaseBy = ( value: number ) => {
-
-    if( isControlled.current ) {
-      return onChange!({ count: value, product });
-    }
 
     const newValue = Math.max( counter + value, 0);
     setCounter( newValue );
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -8,31 +8,31 @@ export const useShoppingCart = () => {
 
     setShoppingCart( oldShoppingCart => {
       // Aplicación sencilla y eficiente
-      // if( count === 0 ){
+      if( count === 0 ){
         
-      //   const { [product.id]:toDelete, ...rest } = oldShoppingCart;
+        const { [product.id]:toDelete, ...rest } = oldShoppingCart;
 
-      //   return { ...rest }
-      // }
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [ product.id ]: { ...product, count }
-      // }
-
-      // Aplicación con control props estricto
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-
-      if( Math.max( productInCart.count + count, 0 ) ){
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart
-        }
+        return { ...rest }
       }
 
-      const { [product.id]:toDelete, ...rest } = oldShoppingCart;
-      return { ...rest }      
+      return {
+        ...oldShoppingCart,
+        [ product.id ]: { ...product, count }
+      }
+
+      // Aplicación con control props estricto
+      // const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
+
+      // if( Math.max( productInCart.count + count, 0 ) ){
+      //   productInCart.count += count;
+      //   return {
+      //     ...oldShoppingCart,
+      //     [product.id]: productInCart
+      //   }
+      // }
+
+      // const { [product.id]:toDelete, ...rest } = oldShoppingCart;
+      // return { ...rest }      
 
     })
   }
