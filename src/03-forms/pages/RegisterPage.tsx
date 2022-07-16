ce62e7e706a747e8118e49b25d97b5414ee613d7
jantonio2 <jesusrochavedia@gmail.com>
2022-07-16 19:52:44 -0400

    Creando useState para el control de los campos del formulario

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,6 +1,21 @@
+import { ChangeEvent, useState } from 'react';
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
+
+  const [registerData, setRegisterData] = useState({
+    name: 'antonio',
+    email: 'antonio@gmail.com',
+    password1: '12345',
+    password2: '12345'
+  });
+
+  const { name, email, password1, password2 } = registerData;
+
+  const onChange = ( event: ChangeEvent<HTMLInputElement> ) => {
+    console.log(event.target.value);
+  }
+
   return (
     <div>
       <h1>RegisterPage</h1>
@@ -10,21 +25,29 @@ export const RegisterPage = () => {
         <input 
           type='text' 
           placeholder='Name'
+          value={ name }
+          onChange={ onChange }
         />
 
         <input 
           type='email' 
           placeholder='Email'
+          value={ email }
+          onChange={ onChange }
         />
 
         <input 
           type='password' 
           placeholder='Password'
+          value={ password1 }
+          onChange={ onChange }
         />
 
         <input 
           type='password' 
           placeholder='Repeat password'
+          value={ password2 }
+          onChange={ onChange }
         />
 
         <button type='submit'>Create</button>
