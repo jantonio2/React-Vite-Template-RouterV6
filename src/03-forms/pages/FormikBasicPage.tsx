73c2207620d681493bdb1d5a0b461615aa7d8ce9
jantonio2 <jesusrochavedia@gmail.com>
2022-07-17 16:10:15 -0400

    Instalando formik y utilizando el custom hook useFormik

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,15 +1,30 @@
+import { useFormik } from 'formik';
 import '../styles/styles.css';
 
 export const FormikBasicPage = () => {
+
+  const { handleChange, handleSubmit, values } = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: ''
+    },
+    onSubmit: values => {
+      console.log(values);
+    }
+  });
+
   return (
     <div>
       <h1>Formik Basic Tutorial</h1>
 
-      <form noValidate>
+      <form onSubmit={ handleSubmit } noValidate>
         <label htmlFor="firstName">First Name</label>
         <input 
           type="text" 
           name="firstName"
+          onChange={ handleChange }
+          value={ values.firstName }
         />
         <span>First name is required</span>
 
@@ -17,6 +32,8 @@ export const FormikBasicPage = () => {
         <input 
           type="text" 
           name="lastName"
+          onChange={ handleChange }
+          value={ values.lastName }
         />
         <span>Last name is required</span>
 
@@ -24,6 +41,8 @@ export const FormikBasicPage = () => {
         <input 
           type="email" 
           name="email"
+          onChange={ handleChange }
+          value={ values.email }
         />
         <span>Email is required</span>
         <span>Check for a valid email format</span>
