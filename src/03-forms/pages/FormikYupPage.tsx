ec342db3026c2cddaaf40dac434f2c29894d7bfc
jantonio2 <jesusrochavedia@gmail.com>
2022-07-17 18:22:29 -0400

    Utilizando getFieldProps para reducir lineas de codigo

--- src/03-forms/pages/FormikYupPage.tsx
+++ src/03-forms/pages/FormikYupPage.tsx
@@ -2,15 +2,9 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import '../styles/styles.css';
 
-interface FormValues {
-  firstName: string;
-  lastName: string;
-  email: string;
-}
-
 export const FormikYupPage = () => {
 
-  const { handleChange, handleSubmit, handleBlur, values, errors, touched } = useFormik({
+  const { handleSubmit, errors, touched, getFieldProps } = useFormik({
     initialValues: {
       firstName: '',
       lastName: '',
@@ -24,7 +18,7 @@ export const FormikYupPage = () => {
                        .max(15, 'Must be 15 characters or less')
                        .required('Required'),
       lastName: Yup.string()
-                      .max(15, 'Must be 15 characters or less')
+                      .max(10, 'Must be 10 characters or less')
                       .required('Required'),
       email: Yup.string()
                  .email('Invalid email address')
@@ -40,30 +34,21 @@ export const FormikYupPage = () => {
         <label htmlFor="firstName">First Name</label>
         <input 
           type="text" 
-          name="firstName"
-          onBlur={ handleBlur }
-          onChange={ handleChange }
-          value={ values.firstName }
+          { ...getFieldProps('firstName') }
         />
         { touched.firstName && errors.firstName && <span>{ errors.firstName }</span> }
 
         <label htmlFor="lastName">Last Name</label>
         <input 
           type="text" 
-          name="lastName"
-          onBlur={ handleBlur }
-          onChange={ handleChange }
-          value={ values.lastName }
+          { ...getFieldProps('lastName') }
         />
         { touched.lastName && errors.lastName && <span>{ errors.lastName }</span> }
 
         <label htmlFor="email">Email Address</label>
         <input 
           type="email" 
-          name="email"
-          onBlur={ handleBlur }
-          onChange={ handleChange }
-          value={ values.email }
+          { ...getFieldProps('email') }
         />
         { touched.email && errors.email && <span>{ errors.email }</span> }
 
