d8fc1c68a92870ccc7daff179457677c5ea0ab3e
jantonio2 <jesusrochavedia@gmail.com>
2022-07-17 18:05:06 -0400

    Utilizando handleBlur para poder mostrar los mensajes de error

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -34,7 +34,7 @@ export const FormikBasicPage = () => {
     return errors;
   }
 
-  const { handleChange, handleSubmit, values } = useFormik({
+  const { handleChange, handleSubmit, handleBlur, values, errors, touched } = useFormik({
     initialValues: {
       firstName: '',
       lastName: '',
@@ -55,29 +55,31 @@ export const FormikBasicPage = () => {
         <input 
           type="text" 
           name="firstName"
+          onBlur={ handleBlur }
           onChange={ handleChange }
           value={ values.firstName }
         />
-        <span>First name is required</span>
+        { touched.firstName && errors.firstName && <span>{ errors.firstName }</span> }
 
         <label htmlFor="lastName">Last Name</label>
         <input 
           type="text" 
           name="lastName"
+          onBlur={ handleBlur }
           onChange={ handleChange }
           value={ values.lastName }
         />
-        <span>Last name is required</span>
+        { touched.lastName && errors.lastName && <span>{ errors.lastName }</span> }
 
         <label htmlFor="email">Email Address</label>
         <input 
           type="email" 
           name="email"
+          onBlur={ handleBlur }
           onChange={ handleChange }
           value={ values.email }
         />
-        <span>Email is required</span>
-        <span>Check for a valid email format</span>
+        { touched.email && errors.email && <span>{ errors.email }</span> }
 
         <button type="submit">Submit</button>
       </form>
