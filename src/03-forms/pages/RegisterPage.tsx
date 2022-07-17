e6c6db7b45dba5e689e2154f51b0dfc3229747f3
jantonio2 <jesusrochavedia@gmail.com>
2022-07-16 20:36:48 -0400

    Creando validaciones para el formulario tradicional

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -4,7 +4,7 @@ import '../styles/styles.css';
 
 export const RegisterPage = () => {
 
-  const { formData, onChange, name, email, password1, password2 } = useForm({
+  const { formData, onChange, name, email, password1, password2, resetForm, isValidEmail } = useForm({
     name: '',
     email: '',
     password1: '',
@@ -28,7 +28,9 @@ export const RegisterPage = () => {
           name='name'
           value={ name }
           onChange={ onChange }
+          className={`${ name.trim().length <= 0 && 'has-error' }`}
         />
+        { name.trim().length <= 0 && <span>Este campo es necesario</span>}
 
         <input 
           type='email' 
@@ -36,7 +38,9 @@ export const RegisterPage = () => {
           name='email'
           value={ email }
           onChange={ onChange }
+          className={`${ !isValidEmail( email ) && 'has-error' }`}
         />
+        { !isValidEmail( email ) && <span>Email no es válido</span>}
 
         <input 
           type='password' 
@@ -45,6 +49,8 @@ export const RegisterPage = () => {
           value={ password1 }
           onChange={ onChange }
         />
+        { password1.trim().length <= 0 && <span>Este campo es necesario</span>}
+        { password1.trim().length < 6 && password1.trim().length > 0 && <span>La contraseña debe tener más de 6 carácteres</span>}
 
         <input 
           type='password' 
@@ -53,8 +59,12 @@ export const RegisterPage = () => {
           value={ password2 }
           onChange={ onChange }
         />
+        { password2.trim().length <= 0 && <span>Este campo es necesario</span>}
+        { password2.trim().length > 0 && password1 !== password2 && <span>Las contraseñas deben ser iguales</span>}
 
         <button type='submit'>Create</button>
+
+        <button type='button' onClick={ resetForm }>Reset Form</button>
 
       </form>
     </div>
